af172cfd4ba078e93f4664e2c1368af3a4dc0838
mosad <abdelrhman.m0s3d@gmail.com>
2020-10-21 01:30:21 +0200

    Standerd library added

---
+++ 02 - LIB/02 - BIT_MATH/BIT_MATH.h
@@ -0,0 +1,18 @@
+/************************************************************/
+/* Author         : A . MOSAD                               */
+/* Date           : 4 / 8 / 2020                            */
+/* version        : V01                                     */
+/************************************************************/
+
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+#define    SET_BIT(REG,BIT)    (REG |= (1 << BIT))
+#define    CLR_BIT(REG,BIT)    (REG &= ~(1 << BIT))
+#define    TOG_BIT(REG,BIT)    (REG ^= ( 1 << BIT))
+#define    CLR_REG(REG)        (REG &= ~(255 << 0))
+#define    SET_REG(REG)        (REG |= ~(255 << 0))
+#define    IS_SET(REG,BIT)     (REG & (1<<BIT))
+#define    GET_BIT(REG,BIT)    ((REG>>BIT) & 1)
+
+#endif
