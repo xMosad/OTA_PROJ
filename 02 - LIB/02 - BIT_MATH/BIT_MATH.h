7d48ed4b29b2c93cd1fb0f1da22ec4637204dadc
osama hijazi <osamahegazi@students.mans.edu.eg>
2020-10-21 21:15:54 +0200

    IS_CLEAR  added

--- 02 - LIB/02 - BIT_MATH/BIT_MATH.h
+++ 02 - LIB/02 - BIT_MATH/BIT_MATH.h
@@ -7,11 +7,17 @@
 #define BIT_MATH_H
 
 #define    SET_BIT(REG,BIT)    (REG |= (1 << BIT))
+
 #define    CLR_BIT(REG,BIT)    (REG &= ~(1 << BIT))
+
 #define    TOG_BIT(REG,BIT)    (REG ^= ( 1 << BIT))
-#define    IS_SET(REG,BIT)     (REG & (1<<BIT))
+
+
 #define    GET_BIT(REG,BIT)    ((REG>>BIT) & 1)
-#define    BIT_IS_CLEAR(REG,BIT) ( !(REG & (1<<BIT)) )
+
+#define    IS_CLEAR(REG,BIT)  (!(REG & (1<<BIT)))
+
+#define    IS_SET(REG,BIT)     (REG & (1<<BIT))
 
 
 #endif
