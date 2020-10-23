bb7d5be24bd2f4a04eb3ef1c1a6177f66b597a77
Omar Gamal <omar.gamal.eldin0@gmail.com>
2020-10-23 02:47:48 +0200

    Added IDs, Removed Trimmiing

--- 03 - MCAL/RCC/RCC_interface.h
+++ 03 - MCAL/RCC/RCC_interface.h
@@ -8,18 +8,67 @@
 
 /**************************************************          Macros          ************************************************************/
 
-/* Bus Id */
-#define RCC_AHB			  0		// AHB Id.
-#define RCC_APB1		  1		// APB1 Id.
-#define RCC_APB2		  2		// APB2 Id.
+// Bus Id
 
-/* Internal High-Speed clock */
+#define RCC_AHB			  0
+#define RCC_APB1		  1
+#define RCC_APB2		  2
 
-#define HSITRIM_NOTRIM	  0		// Internal high-speed clock no trimming.
-#define HSITRIM_POSITIVE  1		// Internal high-speed clock trimming positive steps.
-#define HSITRIM_NEGATIVE  2		// Internal high-speed clock trimming negative steps.
+//peripheral id bits for AHB
+#define MRCC_DMA1_PERIPHERAL     0
+#define MRCC_DMA2_PERIPHERAL     1
+#define MRCC_SRAM_PERIPHERAL     2
+#define MRCC_FLITF_PERIPHERAL    4
+#define MRCC_CRCE_PERIPHERAL     6
+#define MRCC_FSMC_PERIPHERAL     8
+#define MRCC_SDIO_PERIPHERAL     10
 
-/*****************************/
+//peripheral id bits for APB2
+#define MRCC_AFIO_PERIPHERAL     0
+#define MRCC_IOPA_PERIPHERAL     2
+#define MRCC_IOPB_PERIPHERAL     3
+#define MRCC_IOPC_PERIPHERAL     4
+#define MRCC_IOPD_PERIPHERAL     5
+#define MRCC_IOPE_PERIPHERAL     6
+#define MRCC_IOPF_PERIPHERAL     7
+#define MRCC_IOPG_PERIPHERAL     8
+#define MRCC_ADC1_PERIPHERAL     9
+#define MRCC_ADC2_PERIPHERAL     10
+#define MRCC_TIM1_PERIPHERAL     11
+#define MRCC_SPI1_PERIPHERAL     12
+#define MRCC_TIM8_PERIPHERAL     13
+#define MRCC_USART1_PERIPHERAL   14
+#define MRCC_ADC3_PERIPHERAL     15
+#define MRCC_TIM9_PERIPHERAL     19
+#define MRCC_TIM10_PERIPHERAL    20
+#define MRCC_TIM11_PERIPHERAL    21
+
+//peripheral id bits for APB1
+#define MRCC_TIM2_PERIPHERAL     0
+#define MRCC_TIM3_PERIPHERAL     1
+#define MRCC_TIM4_PERIPHERAL     2
+#define MRCC_TIM5_PERIPHERAL     3
+#define MRCC_TIM6_PERIPHERAL     4
+#define MRCC_TIM7_PERIPHERAL     5
+#define MRCC_TIM12_PERIPHERAL    6
+#define MRCC_TIM13_PERIPHERAL    7
+#define MRCC_TIM14_PERIPHERAL    8
+#define MRCC_WWD_PERIPHERAL      11
+#define MRCC_SPI2_PERIPHERAL     14
+#define MRCC_SPI3_PERIPHERAL     15
+#define MRCC_USART2_PERIPHERAL   17
+#define MRCC_USART3_PERIPHERAL   18
+#define MRCC_USART4_PERIPHERAL   19
+#define MRCC_USART5_PERIPHERAL   20
+#define MRCC_I2C1_PERIPHERAL     21
+#define MRCC_I2C2_PERIPHERAL     22
+#define MRCC_USB_PERIPHERAL      23
+#define MRCC_CAN_PERIPHERAL      25
+#define MRCC_BKP_PERIPHERAL      27
+#define MRCC_PWR_PERIPHERAL      28
+#define MRCC_DAC_PERIPHERAL      29
+
+
 
 /****************************************************************************************************************************************/
 
@@ -56,11 +105,6 @@ void RCC_voidDisableClock(u8 Copy_u8BusId,u8 Copy_u8PerId);
 /* O/P:- void 			  																	*/
 void RCC_voidResetRegisters(void);
 /********************************************************************************************/
-
-
-
-u8 RCC_u8SWS(void);
-
 
 /****************************************************************************************************************************************/
 
