4f657ff0b8e6830f0bf8f2154dac45863ca6ef9f
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-10-26 17:53:47 +0200

    Update FPEC_program.c
    Bug Fixed

--- 03 - MCAL/FPEC_V2/FPEC_program.c
+++ 03 - MCAL/FPEC_V2/FPEC_program.c
@@ -51,33 +51,6 @@
 /******************************************************************************
 * Function Definitions
 *******************************************************************************/
-
-/******************************************************************************
-* Function : MFPEC_u8ProgramOneHalfWordFlash()
-*//** 
-* \b Description:
-*
-* This function is used to write code on the flash using flash driver peripheral. 
-*
-* PRE-CONDITION:  Internal clock cycle must be enabled.
-* PRE-CONDITION:  App area must be erased before writing op.
-* 
-* POST-CONDITION: None
-*
-* @param [in]     The address of the code line.
-* @param [in]     Bytes of the code line.
-* 
-* @return 		   The state of writing done ornot.
-*
-* \b Example Example:
-* @code
-* 	MFPEC_u8ProgramOneHalfWordFlash(0x08001000, Code);
-*
-* @endcode
-*
-* @see MFPEC_u8ProgramOneHalfWordFlash
-*
-*******************************************************************************/
 u8 MFPEC_u8ProgramHalfWordFlash(u32 Copy_u32Address, u16 Copy_u16Code)
 {
    u8 Local_u8WritingState = 0;
@@ -98,36 +71,9 @@ u8 MFPEC_u8ProgramHalfWordFlash(u32 Copy_u32Address, u16 Copy_u16Code)
    while(GET_BIT(FPEC->SR, BSY) == SET);
    // End of programming.
    SET_BIT(FPEC->SR, EOP);
-   CLR_BIT(FPEC->CR, PG);
+   CLR_BIT(FPEC->CR, PER);
 }
 
-/******************************************************************************
-* Function : MFPEC_u8ProgramFlash()
-*//** 
-* \b Description:
-*
-* This function is used to write code on the flash using flash driver peripheral. 
-*
-* PRE-CONDITION:  Internal clock cycle must be enabled.
-* PRE-CONDITION:  App area must be erased before writing op.
-* 
-* POST-CONDITION: None
-*
-* @param [in]     The address of the beginning of the app area.
-* @param [in]     An array that contain bytes that the code consists of.
-* @param [in]     The number of half words that the array consists of.
-* 
-* @return 		   void.
-*
-* \b Example Example:
-* @code
-* 	MFPEC_u8ProgramFlash(0x08001000, &Coderray, 16);
-*
-* @endcode
-*
-* @see MFPEC_u8ProgramFlash
-*
-*******************************************************************************/
 void MFPEC_u8ProgramFlash(u32 Copy_u32Address, u16 * Copy_u16Code, u8 Copy_u8Length)
 {
    // Check that no main Flash memory operation is ongoing.
@@ -150,34 +96,10 @@ void MFPEC_u8ProgramFlash(u32 Copy_u32Address, u16 * Copy_u16Code, u8 Copy_u8Len
       while(GET_BIT(FPEC->SR, BSY) == SET);
       // End of programming.
       SET_BIT(FPEC->SR, EOP);
-      CLR_BIT(FPEC->CR, PG);
+      CLR_BIT(FPEC->CR, PER);
    }
 }
 
-/******************************************************************************
-* Function : MFPEC_voidErasePage()
-*//** 
-* \b Description:
-*
-* This function is used to erase page of the flash. 
-*
-* PRE-CONDITION:  Internal clock cycle must be enabled.
-* 
-* POST-CONDITION: None
-*
-* @param [in]     The page number.
-* 
-* @return 		   void.
-*
-* \b Example Example:
-* @code
-* 	MFPEC_voidErasePage(16);
-*
-* @endcode
-*
-* @see MFPEC_voidErasePage
-*
-*******************************************************************************/
 void MFPEC_voidErasePage(u16 Copy_u16PageNumber)
 {
    // Check that no main Flash memory operation is ongoing.
@@ -199,35 +121,8 @@ void MFPEC_voidErasePage(u16 Copy_u16PageNumber)
    while(GET_BIT(FPEC->SR, BSY) == SET);
    // End of programming.
    SET_BIT(FPEC->SR, EOP);
-   CLR_BIT(FPEC->CR, PG);
+   CLR_BIT(FPEC->CR, PER);
 }
-
-/******************************************************************************
-* Function : MFPEC_voidEraseAppArea()
-*//** 
-* \b Description:
-*
-* This function is used to erase the app area. 
-*
-* PRE-CONDITION:  Internal clock cycle must be enabled.
-* PRE-CONDITION:  The first page number in app area must be defined in FPEC_config.
-* PRE-CONDITION:  The last  page number in app area must be defined in FPEC_config.
-* 
-* POST-CONDITION: None
-*
-* @param [in]     void.
-* 
-* @return 		   void.
-*
-* \b Example Example:
-* @code
-* 	MFPEC_voidEraseAppArea();
-*
-* @endcode
-*
-* @see MFPEC_voidEraseAppArea
-*
-*******************************************************************************/
 void MFPEC_voidEraseAppArea(void)
 {
    for(u8 Local_u8Page = FIRST_PAGE_NUMBER_IN_APP_AREA;Local_u8Page <= LAST_PAGE_NUMBER_IN_APP_AREA;Local_u8Page++)
